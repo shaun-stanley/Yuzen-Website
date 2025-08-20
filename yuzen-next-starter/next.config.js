819535d028fac3e61b72742a3f5b88c2d364d362
Shaun Stanley <71328282+shaun-stanley@users.noreply.github.com>
2025-08-21 00:36:42 +0530

    chore: enforce type-safe routes

--- yuzen-next-starter/next.config.js
+++ yuzen-next-starter/next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { typedRoutes: true },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  typedRoutes: true,
 };
 module.exports = nextConfig;
